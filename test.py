0b00a354939fcfe2adecc3b2511622dfcc5a33d2
deepjyotisaha85 <57674437+deepjyotisaha85@users.noreply.github.com>
2020-05-09 10:03:05 +0530

    Add files via upload

--- test.py
+++ test.py
@@ -48,24 +48,12 @@ from matplotlib import pyplot as plt
 
 def loadmodel():
     custom_objects = {'BilinearUpSampling2D': BilinearUpSampling2D, 'depth_loss_function': None}
-
     print('Loading model...')
-
     # Load model into GPU / CPU
     model = load_model('nyu.h5', custom_objects=custom_objects, compile=False)
-
     return model
 
 def generate_images(model, inpath, outpath, start, end):
-    # Custom object needed for inference and training
-    custom_objects = {'BilinearUpSampling2D': BilinearUpSampling2D, 'depth_loss_function': None}
-
-    print('Loading model...')
-
-    # Load model into GPU / CPU
-    #model = load_model('nyu.h5', custom_objects=custom_objects, compile=False)
-
-    #print('\nModel loaded ({0}).'.format("nyu.h5"))
 
     inputs = load_images(inpath, start, end)
     print('Loaded images from :', start, ' till: ', end)
